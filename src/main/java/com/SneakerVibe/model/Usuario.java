a8a32b2abac30b3e198a39374272e04e27eba7ad
PV-Alumno <PV-Alumno@duoc.acad>
2025-11-13 15:55:40 -0300

    tablas

--- src/main/java/com/SneakerVibe/model/Usuario.java
+++ src/main/java/com/SneakerVibe/model/Usuario.java
@@ -7,21 +7,31 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Table(name="usuario")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Usuario {
-     @Id
+
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nombre;
     private String apellido;
+    
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String contrasena;
     private String direccion;
+    
+    @Column(name =  "nro_domicio")
     private String numDomicilio;
     private String region;
     private String comuna;
+
+    @Column(nullable = false)
     private Boolean esAdmin;
 }
    
