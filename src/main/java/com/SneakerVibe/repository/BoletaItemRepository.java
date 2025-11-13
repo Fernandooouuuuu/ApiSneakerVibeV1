d33142fadb82b89493563e2bcce0c0f4474cfa2e
dariodelagb <dar.jeria@duocuc.cl>
2025-11-13 16:29:35 -0300

    cambios repository

---
+++ src/main/java/com/SneakerVibe/repository/BoletaItemRepository.java
@@ -0,0 +1,9 @@
+package com.SneakerVibe.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.SneakerVibe.model.BoletaItem;
+
+public interface BoletaItemRepository extends JpaRepository<BoletaItem, Long>{
+    
+}
